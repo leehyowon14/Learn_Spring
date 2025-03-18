967f3b93f609bb3dca8268722ab196b81bf1b8a4
leehyowon14 <leehyowon14@gmail.com>
2025-03-18 23:33:32 +0900

    feat: 프로젝트 파일 업로드
    Spring 프로젝트 파일을 업로드합니다!

---
+++ src/main/java/com/example/BabyLionIsRoaring/BabyLionIsRoaringApplication.java
@@ -0,0 +1,13 @@
+package com.example.BabyLionIsRoaring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyLionIsRoaringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyLionIsRoaringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BabyLionIsRoaring/BabyLionIsRoaringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BabyLionIsRoaring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyLionIsRoaringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
